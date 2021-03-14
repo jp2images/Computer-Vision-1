4f6b8b6669fd4f2277248545a243450c08623ca5
Jeff <jeff_patterson@outlook.com>
2021-03-13 18:50:28 -0600

    Working on Assignment 2 Will it be a sln or cmake? Waiting for an answer

---
+++ Assignment2/submission.cpp
@@ -0,0 +1,78 @@
+﻿// Assignment2.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
+#include "submission.h"
+
+using namespace std;
+
+
+//Some global variables just for the sake of ease during training.
+cv::Point center, circumference;
+cv::Mat source;
+//Prototype the function for when we add files
+void drawCircle(int, int, int, int, void*);
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+
+
+        //read in the image
+        source = cv::imread(DATA_PATH + "sample.jpg");
+
+        cv::Mat clearedImage = source.clone();
+        cv::namedWindow("Window");
+
+        cv::setMouseCallback("Window", drawCircle);
+
+        int k = 0;
+
+        while (k != 27) { //not equal to the Esc key pressed
+            cv::imshow("Window", source);
+            cv::putText(source, "Choose the circle center and drag.",
+                cv::Point(10, 20), FONT_HERSHEY_SIMPLEX, 0.7,
+                Scalar(255, 255, 255), 1, cv::LINE_AA);
+            cv::putText(source, "Press Esc to exit and c to clear.",
+                cv::Point(10, 45), FONT_HERSHEY_SIMPLEX, 0.7,
+                Scalar(255, 255, 255), 1, cv::LINE_AA);
+
+            k = cv::waitKey(20) & 0xFF;
+
+            if (k == 99) clearedImage.copyTo(source);
+        }
+
+        return 0;
+    }
+
+    void drawCircle(int action, int xPos, int yPos, int flags, void* userdata) {
+
+        switch (action) {
+        case cv::EVENT_LBUTTONDOWN:
+            center = cv::Point(xPos, yPos);
+            cv::circle(source, center, 1, cv::Scalar(255, 255, 0), 2, cv::LINE_AA);
+            break;
+
+        case cv::EVENT_LBUTTONUP:
+            //calculate the circles radius
+            circumference = cv::Point(xPos, yPos);
+            float radius = sqrt(pow(center.x - circumference.x, 2) + pow(center.y - circumference.y, 2));
+
+            //draw the circle
+            cv::circle(source, center, radius, cv::Scalar(0, 255, 255), 2, cv::LINE_AA);
+
+            cv::imshow("Window", source);
+            break;
+        }
+    }
+}
---
+++ Week 2/Assignment2Annotations/main.cpp
@@ -0,0 +1,85 @@
+
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+
+#include "dataPath.hpp"
+
+using namespace std;
+using namespace cv;
+
+//Some global variables just for the sake of ease during training.
+cv::Point rectStart, rectEnd;
+cv::Mat source;
+//Prototype the function for when we add files
+void drawBox(int, int, int, int, void*);
+
+const int ESC_KEY = 27;
+const int C_KEY = 99;
+
+
+int main() {
+
+    //read in the image
+    source = cv::imread(DATA_PATH + "sample.jpg");
+
+    string croppedImageName = DATA_PATH + "cropped.jpg";
+
+    cv::Mat clearedImage = source.clone();
+    cv::Mat croppedImage = source.clone();
+
+    cv::namedWindow("Window");
+
+    cv::setMouseCallback("Window", drawBox);
+
+    int k = 0;
+
+
+    while (k != ESC_KEY) { //not equal to the Esc key pressed
+        cv::imshow("Window", source);
+
+        cv::putText(source, "Choose the the box starting point and drag.",
+            cv::Point(10, 20), FONT_HERSHEY_SIMPLEX, 0.7, Scalar(255, 255, 255), 1, cv::LINE_AA);
+        cv::putText(source, "Press Esc to save the last box as a crop",
+            cv::Point(10, 45), FONT_HERSHEY_SIMPLEX, 0.7, Scalar(255, 255, 255), 1, cv::LINE_AA);
+        cv::putText(source, "and Exit. Press c to clear and start over.",
+            cv::Point(10, 70), FONT_HERSHEY_SIMPLEX, 0.7, Scalar(255, 255, 255), 1, cv::LINE_AA);
+
+        k = cv::waitKey(20) & 0xFF;
+
+        if (k == C_KEY) clearedImage.copyTo(source);
+    }
+
+    croppedImage = source(cv::Range(rectStart.x, rectEnd.x), cv::Range(rectStart.y, rectEnd.y));
+
+    cv::imwrite(croppedImageName, croppedImage);
+    cv::imshow("Cropped Image", croppedImage);
+    cv::waitKey(0);
+    cv::destroyAllWindows();
+    return 0;
+}
+
+
+
+void drawBox(int action, int xPos, int yPos, int flags, void* userdata) {
+
+    switch (action) {
+    case cv::EVENT_LBUTTONDOWN:
+        rectStart = cv::Point(xPos, yPos);
+        cv::circle(source, rectStart, 1, cv::Scalar(255, 255, 0), 2, cv::LINE_AA);
+        break;
+
+    case cv::EVENT_LBUTTONUP:
+        rectEnd = cv::Point(xPos, yPos);
+
+        //draw the circle
+        cv::rectangle(source, rectStart, rectEnd, cv::Scalar(0, 255, 255), 2, cv::LINE_AA);
+
+        cv::imshow("Window", source);
+        break;
+    }
+}
