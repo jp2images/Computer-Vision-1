2e3e1c6e5964fbd39ac752bb5d44e34c7b8e33ed
Jeff <jeff_patterson@outlook.com>
2021-03-20 11:41:10 -0500

    video writes and now to work on resizing

--- Week 3/AssignmentSe/main.cpp
+++ Week 3/AssignmentSe/main.cpp
@@ -40,12 +40,11 @@ int main() {
     int height = demoImage.size().height;
     int width = demoImage.size().width;
 
-    cv::Mat dilatedCrossKernel;
-    cv::dilate(demoImage, dilatedCrossKernel, element);
+    //cv::Mat dilatedCrossKernel;
+    //cv::dilate(demoImage, dilatedCrossKernel, element);
     //cout << dilatedCrossKernel << endl << endl;
     //quickShow(dilatedCrossKernel * 255);
 
-    //
     int border = ksize / 2;
     cv::Mat paddedDemoImage = cv::Mat::zeros(cv::Size(height + (border * 2), width + (border * 2)), CV_8UC1);
     cv::copyMakeBorder(demoImage, paddedDemoImage, border, border, border, border, cv::BORDER_CONSTANT, 0);
@@ -56,46 +55,80 @@ int main() {
 
     double minVal, maxVal;
 
+    string videoName = "dilationScratch.avi";
     int vidHeight = paddedDilatedImage.size().height;
     int vidWidth = paddedDilatedImage.size().width;
-    int videoFPS = 10;
+    int videoFPS = 5;
 
-    cv::VideoWriter videoOut("dilationScratch.mp4", 
-        //cv::VideoWriter::fourcc('M', 'P', 'G', '4'),
-        //videoFPS, cv::Size(height + 2, width + 2));
+
+    cv::VideoWriter videoOut(videoName,
         cv::VideoWriter::fourcc('M', 'J', 'P', 'G'),
-        videoFPS, cv::Size(vidHeight, vidWidth));
-
+        videoFPS, cv::Size(vidHeight, vidWidth), false);
 
     if (!videoOut.isOpened()) {
+
         cout << endl << "Video Writer is not OPEN" << endl << endl;
         return -1;
     }
 
+    //quickShow(paddedDemoImage * 255);
+    //quickShow(element * 255);
+    cv::Mat bigPicture;
+
+    videoOut.write(paddedDemoImage * 255);
     for (int h_i = border; h_i < height + border; h_i++) {
         for (int w_i = border; w_i < width + border; w_i++) {
             if (demoImage.at<uchar>(h_i - border, w_i - border)) {
-                
-                cv::bitwise_or(paddedDemoImage(
+                //Or the structuring element with the the image and write the result to the mask.
+                cv::bitwise_or(paddedDilatedImage(
                     cv::Range(h_i - border, h_i + border + 1),
                     cv::Range(w_i - border, w_i + border + 1)), element, mask);
-
-                mask.copyTo(paddedDemoImage(cv::Range(h_i - border, h_i + border + 1),
+                //Copy the mask back to the image
+                mask.copyTo(paddedDilatedImage(cv::Range(h_i - border, h_i + border + 1),
                     cv::Range(w_i - border, w_i + border + 1)));
 
+                cv::resize(paddedDilatedImage, resizedFrame, cv::Size(50, 50), 0, 0, cv::INTER_CUBIC);
+                videoOut.write(resizedFrame * 255);
+                cv::imshow("video", resizedFrame * 255);
 
-                videoOut.write(paddedDemoImage * 255);
-
-                cv::imshow("video", paddedDemoImage * 255);
-                cv::waitKey(100);                
+                //videoOut.write(paddedDilatedImage * 255);
+                //videoOut.write(paddedDilatedImage * 255);
+                //cv::imshow("video", paddedDilatedImage * 255);
+                //cv::imshow("video", paddedDilatedImage * 255);
             }
-            cv::imshow("video", paddedDemoImage * 255);
-            cv::waitKey(10);
-        }        
-    }            
+            cv::waitKey(100);                
+        } 
+    }      
+    videoOut.write(paddedDilatedImage * 255);
+
     videoOut.release();
     cv::waitKey(0);
     cv::destroyWindow("video");
+
+    //Play the video back
+    cv::VideoCapture myVid(videoName);
+
+    if (!myVid.isOpened()) {
+        cout << endl << "Error opening video stream or file" << endl << endl;
+        return -1;
+    }
+    cv::Mat frame;
+    int videoWidth = myVid.get(cv::CAP_PROP_FRAME_WIDTH);
+    int videoHeight = myVid.get(cv::CAP_PROP_FRAME_HEIGHT);
+    cout << endl << endl << "Width: " << videoWidth << " Height: " << videoHeight << endl << endl;
+    //Start at a different position of the video
+    myVid.set(cv::CAP_PROP_POS_MSEC, 0);
+
+    while (myVid.isOpened()) {
+        myVid >> frame;
+
+        if (frame.empty()) {
+            break;
+        }
+        cv::imshow("Frame", frame);
+        cv::waitKey(500);
+    }
+
     return 0;
 }
 
