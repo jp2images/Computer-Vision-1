0f70b5f4139cc1af66849ed894c45f146bfc44ca
Jeff <jeff_patterson@outlook.com>
2021-03-21 18:43:09 -0500

    Trying to get the erosion to work. Not having much luck understanding what I need to do.

--- Week 3/AssignmentSe/main.cpp
+++ Week 3/AssignmentSe/main.cpp
@@ -51,67 +51,112 @@ int main() {
     cv::Mat paddedDilatedImage = paddedDemoImage.clone();
     cv::Mat mask;
     cv::Mat resizedFrame;
-    cv::Mat imageColored;
 
-    cv::Rect rectImage;
-
-    string videoName = "dilationScratch.avi";
-    int vidHeight = paddedDilatedImage.size().height;
-    int vidWidth = paddedDilatedImage.size().width;
+    string videoDilationName = "dilationScratch.avi";
     int videoFPS = 10;
 
-    cv::VideoWriter videoOut(videoName,
+    cv::VideoWriter videoDilationOut(videoDilationName,
         cv::VideoWriter::fourcc('M', 'J', 'P', 'G'),
-        //videoFPS, cv::Size(vidHeight, vidWidth), false);
-        videoFPS, cv::Size(50, 50), false);
+        videoFPS, cv::Size(50, 50), true);
 
-    if (!videoOut.isOpened()) {
-
-        std::cout << endl << "Video Writer is not OPEN" << endl << endl;
+    if (!videoDilationOut.isOpened()) {
+        std::cout << endl << "Dilation Video Writer is not OPEN" << endl << endl;
         return -1;
     }
-
-    cv::resize(paddedDilatedImage * 255, resizedFrame, cv::Size(50, 50), 0, 0, cv::INTER_CUBIC); 
-    //Write a few frames of the original image before we start dilating.
-    for (int x = 0; x < 5; x++) {
-        videoOut.write(resizedFrame);
-    }
-
+    
     int x = demoImage.size().height;
     int y = demoImage.size().width;
-    cv::Rect originalImageRect(1, 1, x, y);
-
+   
     cv::Mat imageCropped;
 
     //Begin dilation of the image.
+    //for (int h_i = border; h_i < height + border; h_i++) {
+    //    for (int w_i = border; w_i < width + border; w_i++) {
+    //        if (demoImage.at<uchar>(h_i - border, w_i - border)) {
+    //            //Or the structuring element with the the image and write the result to the mask.
+    //            cv::bitwise_or(paddedDilatedImage(
+    //                cv::Range(h_i - border, h_i + border + 1),
+    //                cv::Range(w_i - border, w_i + border + 1)), element, mask);
+    //            //Copy the mask back to the image
+    //            mask.copyTo(paddedDilatedImage(cv::Range(h_i - border, h_i + border + 1),
+    //                cv::Range(w_i - border, w_i + border + 1)));
+    //        }
+    //        imageCropped = paddedDilatedImage(cv::Rect(1, 1, x, y));
+    //        cv::resize(imageCropped * 255, resizedFrame, cv::Size(50, 50), 0, 0, cv::INTER_NEAREST);
+    //        cv::cvtColor(resizedFrame, resizedFrame, cv::COLOR_GRAY2BGR);
+    //        videoDilationOut.write(resizedFrame);
+    //        cv::imshow("Video Dilation", resizedFrame);
+    //        cv::waitKey(50);
+    //    } 
+    //}      
+
+    //videoDilationOut.release();
+    //cv::waitKey(0);
+    //cv::destroyWindow("Video Dilation");
+
+
+    cv::Mat invertedMask = cv::Mat::ones(cv::Size(3, 3), CV_8U);
+    cv::Mat erosionElement;// = cv::getStructuringElement(cv::MORPH_CROSS, cv::Size(3, 3));
+
+    cv::bitwise_xor(element, invertedMask, erosionElement);
+    cout << erosionElement << endl << endl;
+
+    cv::Mat paddedErodedImage = paddedDemoImage.clone();
+    //cv::Mat mask;
+    //cv::Mat resizedFrame;
+
+    string videoErosionName = "erosionScratch.avi";
+    //int videoFPS = 10;
+
+    cv::VideoWriter videoErosionOut(videoErosionName,
+        cv::VideoWriter::fourcc('M', 'J', 'P', 'G'),
+        videoFPS, cv::Size(50, 50), true);
+
+    if (!videoErosionOut.isOpened()) {
+        std::cout << endl << "Erosion Video Writer is not OPEN" << endl << endl;
+        return -1;
+    }
+
+    double  minVal, maxVal;
+    cv::Point erodeRow, erodeCol;
+
+    //Begin erosion of the image.
     for (int h_i = border; h_i < height + border; h_i++) {
         for (int w_i = border; w_i < width + border; w_i++) {
             if (demoImage.at<uchar>(h_i - border, w_i - border)) {
+
                 //Or the structuring element with the the image and write the result to the mask.
-                cv::bitwise_or(paddedDilatedImage(
+                cv::bitwise_and(paddedErodedImage(
                     cv::Range(h_i - border, h_i + border + 1),
                     cv::Range(w_i - border, w_i + border + 1)), element, mask);
-                //Copy the mask back to the image
-                mask.copyTo(paddedDilatedImage(cv::Range(h_i - border, h_i + border + 1),
-                    cv::Range(w_i - border, w_i + border + 1)));
+                
+                cv::minMaxLoc(mask, &minVal, &maxVal);
+                cout << "Max:" << maxVal << endl << endl;
+                if (maxVal == 1) {
+                    //Copy the mask back to the image
+                    mask.copyTo(paddedErodedImage(
+                        cv::Range(h_i - border, h_i + border + 1),
+                        cv::Range(w_i - border, w_i + border + 1)));
+                }
             }
-            imageCropped = paddedDilatedImage(originalImageRect);
-            cv::resize(imageCropped, resizedFrame, cv::Size(50, 50), 0, 0, cv::INTER_NEAREST);
 
-            //Make the image size larger
-            cv::cvtColor(resizedFrame, imageColored, cv::COLOR_GRAY2BGR);
-            videoOut.write(imageColored * 255);
-            cv::imshow("video", imageColored * 255);
-            cv::waitKey(50);
-        } 
-    }      
+            imageCropped = paddedErodedImage(cv::Rect(1, 1, x, y));
+            cv::resize(imageCropped * 255, resizedFrame, cv::Size(50, 50), 0, 0, cv::INTER_NEAREST);
+            cv::cvtColor(resizedFrame, resizedFrame, cv::COLOR_GRAY2BGR);
+            videoErosionOut.write(resizedFrame);
+            cv::imshow("Video Erosion", resizedFrame);
+            cv::waitKey(20);
+        }
+    }
 
-    videoOut.release();
+    videoErosionOut.release();
     cv::waitKey(0);
-    cv::destroyWindow("video");
+    cv::destroyWindow("Video Erosion");
+
+
 
     //Play the video back
-    cv::VideoCapture myVid(videoName);
+    cv::VideoCapture myVid(videoDilationName);
 
     if (!myVid.isOpened()) {
         std::cout << std::endl << "Error opening video stream or file" << std::endl << std::endl;
