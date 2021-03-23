e8b42e5e450fa6a036d49fab5bb5fb244fce5988
Jeff <jeff_patterson@outlook.com>
2021-03-22 19:35:43 -0500

    Done for the day, no progress

--- Week 3/AssignmentSe/main.cpp
+++ Week 3/AssignmentSe/main.cpp
@@ -95,18 +95,10 @@ int main() {
     //cv::destroyWindow("Video Dilation");
 
 
-    cv::Mat invertedMask = cv::Mat::ones(cv::Size(3, 3), CV_8U);
-    cv::Mat erosionElement;// = cv::getStructuringElement(cv::MORPH_CROSS, cv::Size(3, 3));
-
-    cv::bitwise_xor(element, invertedMask, erosionElement);
-    cout << erosionElement << endl << endl;
+    cv::Mat erosionMask = cv::Mat::zeros(cv::Size(3, 3), CV_8U);
 
     cv::Mat paddedErodedImage = paddedDemoImage.clone();
-    //cv::Mat mask;
-    //cv::Mat resizedFrame;
-
     string videoErosionName = "erosionScratch.avi";
-    //int videoFPS = 10;
 
     cv::VideoWriter videoErosionOut(videoErosionName,
         cv::VideoWriter::fourcc('M', 'J', 'P', 'G'),
@@ -118,27 +110,28 @@ int main() {
     }
 
     double  minVal, maxVal;
-    cv::Point erodeRow, erodeCol;
 
     //Begin erosion of the image.
     for (int h_i = border; h_i < height + border; h_i++) {
         for (int w_i = border; w_i < width + border; w_i++) {
             if (demoImage.at<uchar>(h_i - border, w_i - border)) {
 
-                //Or the structuring element with the the image and write the result to the mask.
-                cv::bitwise_and(paddedErodedImage(
-                    cv::Range(h_i - border, h_i + border + 1),
-                    cv::Range(w_i - border, w_i + border + 1)), element, mask);
+                //cv::Mat roi = paddedErodedImage(cv::Range(h_i - border, h_i + border + 1), cv::Range(w_i - border, w_i + border + 1));
+
+                //AND the structuring element with the the image and write the result to the mask.
+                cv::bitwise_and(paddedErodedImage(cv::Range(h_i - border, h_i + border + 1), cv::Range(w_i - border, w_i + border + 1)), element, erosionMask);
+                cv::minMaxLoc(element, &minVal, &maxVal);
                 
-                cv::minMaxLoc(mask, &minVal, &maxVal);
-                cout << "Max:" << maxVal << endl << endl;
-                if (maxVal == 1) {
-                    //Copy the mask back to the image
-                    mask.copyTo(paddedErodedImage(
-                        cv::Range(h_i - border, h_i + border + 1),
-                        cv::Range(w_i - border, w_i + border + 1)));
-                }
+               // cout << "Max:" << maxVal << endl << endl;
+                //Copy the mask back to the image
+                /*erosionMask.copyTo(paddedErodedImage(
+                    cv::Range(h_i - border, h_i + border + 1),
+                   cv::Range(w_i - border, w_i + border + 1)));*/
+
+                paddedErodedImage(cv::Range(h_i - border, h_i + border + 1), cv::Range(w_i - border, w_i + border + 1)) = maxVal;
+
             }
+
 
             imageCropped = paddedErodedImage(cv::Rect(1, 1, x, y));
             cv::resize(imageCropped * 255, resizedFrame, cv::Size(50, 50), 0, 0, cv::INTER_NEAREST);
